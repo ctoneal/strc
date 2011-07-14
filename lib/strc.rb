2c497965f0a4a3ccdef20e96b31716332308534a
Chris <ctoneal@gmail.com>
2011-07-14 09:06:57 -0500

    added smembers, made the other set commands a little more readable

--- lib/strc.rb
+++ lib/strc.rb
@@ -21,15 +21,17 @@ class STRC
 		if args.length < 1
 			raise STRC::Exception.new "Wrong number of arguments (1 for 2)"
 		end
-		if @dict.has_key?(key)
-			if @dict[key].class == Array
-				@dict[key] << args
-			else
-				raise STRC::Exception.new "ERR Operation against a key holding the wrong kind of value"
-			end
-		else
-			@dict[key] = args
-		end
+		# if @dict.has_key?(key)
+		# 	if @dict[key].class == Array
+		# 		@dict[key] << args
+		# 	else
+		# 		raise STRC::Exception.new "ERR Operation against a key holding the wrong kind of value"
+		# 	end
+		# else
+		# 	@dict[key] = args
+		# end
+		set = smembers(key)
+		@dict[key] = set + args
 		return args.length
 	end
 
@@ -45,14 +47,16 @@ class STRC
 
 	# Determine if the given value is a member of the set at key
 	def sismember(key, member)
-		if @dict.has_key?(key)
-			if @dict[key].class == Array
-				return true unless @dict[key].index(member).nil?
-			else
-				raise STRC::Exception .new "ERR Operation against a key holding the wrong kind of value"
-			end
-		end
-		return false
+		# if @dict.has_key?(key)
+		# 	if @dict[key].class == Array
+		# 		return true unless @dict[key].index(member).nil?
+		# 	else
+		# 		raise STRC::Exception .new "ERR Operation against a key holding the wrong kind of value"
+		# 	end
+		# end
+		set = smembers(key)
+		return !set.index(member).nil?
+		#return false
 	end
 
 	# Returns an array of members of the set
@@ -70,14 +74,16 @@ class STRC
 
 	# Gets the length of a set
 	def scard(key)
-		if @dict.has_key?(key)
-			if @dict[key].class == Array
-				@dict[key].length
-			else
-				raise STRC::Exception.new "ERR Operation against a key holding the wrong kind of value"
-			end
-		else
-			return 0
-		end
+		# if @dict.has_key?(key)
+		# 	if @dict[key].class == Array
+		# 		@dict[key].length
+		# 	else
+		# 		raise STRC::Exception.new "ERR Operation against a key holding the wrong kind of value"
+		# 	end
+		# else
+		# 	return 0
+		# end
+		set = smembers(key)
+		return set.length
 	end
 end
